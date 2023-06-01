1866367314d864ed24e9ecbd4a51305614e8381b
alojine <76651648+alojine@users.noreply.github.com>
2023-06-02 02:43:10 +0300

    styles

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png';
 
 export const Header = () => {
   return (
     <header>
         <Navbar bg="light" varient="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <img src={logo} alt="ProShop" />
+                    ProShop
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
