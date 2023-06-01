0441b126405c4bfadabf9d9a8592c19d3d488a96
alojine <76651648+alojine@users.noreply.github.com>
2023-06-02 01:24:58 +0300

    Footer and Header components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+
+export const Header = () => {
+  return (
+    <header>
+        <Navbar bg="light" varient="dark" expand="md" collapseOnSelect>
+            <Container>
+                <Navbar.Brand href="/">ProShop</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="/cart"><FaShoppingCart />Cart</Nav.Link>
+                        <Nav.Link href="/login"><FaUser />Sign In</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
