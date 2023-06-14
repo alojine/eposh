eac45a297951ee207f06b92c5fdbf29ec98baf40
alojine <76651648+alojine@users.noreply.github.com>
2023-06-14 16:25:20 +0300

    Loader and Message components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 const HomeScreen = () => {
@@ -9,19 +11,23 @@ const HomeScreen = () => {
   return (
     <>
       { isLoading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <div>{ error?.data?.message || error?.error }</div>
-        ) : (<>
-        <h1>Latest PRoducts</h1>
-        <Row>
-            { products.map((product) => (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product} />
-                </Col>
-            )) }
-        </Row>
-      </>) }
+        <Message variant="danger">
+          { error?.data?.message || error?.error }
+        </Message>
+      ) : (
+        <>
+          <h1>Latest PRoducts</h1>
+          <Row>
+              { products.map((product) => (
+                  <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                      <Product product={product} />
+                  </Col>
+              )) }
+          </Row>
+        </>
+      ) }
         
     </>
   )
