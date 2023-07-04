83a058b71fa88b7d1655a3b2a571df4579fbd7ef
alojine <76651648+alojine@users.noreply.github.com>
2023-07-04 19:43:52 +0300

    user controller

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,20 @@
+import express from "express";
+const router = express.Router();
+import { authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    deleteUser,
+    getUserByID,
+    updateUser 
+} from '../controllers/userController.js';
+
+router.route('/').post(registerUser).get(getUsers);
+router.post('/logout', logoutUser);
+router.post('/login', authUser);
+router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router.route('/:id').get(getUserByID).delete(deleteUser).put(updateUser);
+
+export default router;
--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import { FaTrash } from 'react-icons/fa';
 import Message from '../components/Message';
-import { addToCart } from '../slices/cartSlice';
+import { addToCart, removeFromCart } from '../slices/cartSlice';
 
 const CartScreen = () => {
     const navigate = useNavigate();
@@ -15,6 +15,14 @@ const CartScreen = () => {
 
     const addToCartHandler = async (product, qty) => {
         dispatch(addToCart({...product, qty}));
+    }
+
+    const removeFromCartHandler = async (id) => {
+        dispatch(removeFromCart(id));
+    }
+
+    const checkOutHandler = () => {
+        navigate(`/login?redirect=/shipping`);
     }
 
   return <Row>
@@ -52,7 +60,7 @@ const CartScreen = () => {
                                 </Form.Control>
                             </Col>
                             <Col md={2}>
-                                <Button type='button' variant='light'>
+                                <Button type='button' variant='light' onClick={ () => removeFromCartHandler(item._id) }>
                                     <FaTrash />
                                 </Button>
                             </Col>
@@ -72,7 +80,12 @@ const CartScreen = () => {
                     ${ cartItems.reduce( (acc, item) => acc + item.qty * item.price, 0).toFixed(2) }
                 </ListGroup.Item>
                 <ListGroup.Item>
-                    <Button type='button' className='btn-block' disabled={ cartItems.length === 0}>
+                    <Button 
+                        type='button' 
+                        className='btn-block' 
+                        disabled={ cartItems.length === 0}
+                        onClick={ checkOutHandler}
+                        >
                         Proceed To Checkout
                     </Button>
                 </ListGroup.Item>
